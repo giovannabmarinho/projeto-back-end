b81d37ca4dffb77ce10f8b032fe9b6dd24c0cc1c
Giovanna <giovannabmarinho@outlook.com.br>
2025-11-16 15:11:22 -0300

    criando servico-planos-ativos

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -12,6 +12,14 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
           host: '127.0.0.1',
           port: 3001,
         },
+      },
+      {
+        name: 'SERVICO_PLANOS_ATIVOS',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3002,
+        },
       }
     ])
   ],
---
+++ servico-planos-ativos/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3002,
+    },
+  });
+  await app.listen().then(() => {
+    console.log('Servico de Planos Ativos iniciado na porta 3002')
+  });
+}
+
+bootstrap();
